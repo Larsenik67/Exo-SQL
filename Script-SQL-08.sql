e41122f468fc2cb257344b7d96621f9606f03c4c
Larsenik67 <ylachqar@gmail.com>
2021-11-01 21:10:59 +0100

    Modification de certain exos

--- Script-SQL-08.sql
+++ Script-SQL-08.sql
@@ -1,7 +1,9 @@
-SELECT nom_personnage, qte
-FROM prendre_casque pc 
+SELECT sqte.nom_personnage, MAX(sqte.quantite) AS max_qte
+FROM (SELECT p.nom_personnage, SUM(pc.qte) as quantite
+FROM prendre_casque pc
 INNER JOIN personnage p ON pc.id_personnage = p.id_personnage
 INNER JOIN bataille b ON pc.id_bataille = b.id_bataille
-WHERE nom_bataille = "Bataille du village gaulois"
-ORDER BY qte DESC
-LIMIT 3
+WHERE b.nom_bataille = "Bataille du village gaulois"
+GROUP BY p.nom_personnage) sqte
+GROUP BY sqte.nom_personnage
+ORDER BY max_qte DESC
--- Script-SQL-11.sql
+++ Script-SQL-11.sql
@@ -1,4 +1,4 @@
-SELECT nom_type_casque, SUM(cout_casque) AS prix
+SELECT nom_type_casque, COUNT(id_casque), SUM(cout_casque) AS prix
 FROM casque c
 INNER JOIN type_casque tc ON c.id_type_casque = tc.id_type_casque
 GROUP BY nom_type_casque
