f135eadacdd3c8751b8779feb2f81f0ced7feecd
Larsenik67 <ylachqar@gmail.com>
2021-11-08 11:50:05 +0100

    Optimisation

--- Requete-SQL-G (BONUS).sql
+++ Requete-SQL-G (BONUS).sql
@@ -1,4 +1,4 @@
-SELECT p.nom_personnage, LOWER(CONCAT(SUBSTRING(p.nom_personnage, 1, 3), ".", SUBSTRING(l.nom_lieu, 1, 4), "@gaulois.fr")) AS mail
+SELECT p.nom_personnage, LOWER(CONCAT(LEFT(p.nom_personnage, 3), ".", LEFT(l.nom_lieu, 4), "@gaulois.fr")) AS mail
 FROM personnage p 
 INNER JOIN lieu l ON p.id_lieu = l.id_lieu
 ORDER BY p.nom_personnage
